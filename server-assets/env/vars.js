520d8f2ca34b19cfa6419f13cc20b48537547f50
Jason Turner <jasonalmaturner@gmail.com>
2014-12-12 12:35:36 -0700

    adding linkedIn tokens and stuff

--- server-assets/env/vars.js
+++ server-assets/env/vars.js
@@ -1,4 +1,10 @@
 module.exports = {
 	expressSecret: 'quando omni flunkus moritati',
-	expressPort: 9001
+	expressPort: 9001,
+	linkedIn: {
+		APIKey: '75td24s9br4664',
+		secretKey: 'ScKz5hqF60wq1RgI',
+		oAuthUserToken: '31c766bb-6e23-4e86-801e-9f292118984e',
+		oAuthUserSecret: 'a0062816-0663-4141-a1a4-4dcae1a90cec'
+	}
 }
